6b5deeeec837e4881df671c8ebc9f32da079ca10
Caryourdaysbm <adeyemioluwakayode73@gmail.com>
2022-04-07 20:02:13 -0700

    malloc free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,9 @@
-#include "holberton.h"
+#include <holberton.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
 
 /**
 * simple_print_buffer - prints buffer in hexa
