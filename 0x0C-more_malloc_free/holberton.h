49e4c5831ca69fe137089f26a1e96bf30fec90d0
Caryourdaysbm <adeyemioluwakayode73@gmail.com>
2022-04-07 20:17:06 -0700

    malloc free

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,5 +1,5 @@
-#ifndef _HOLBERTON_H
-#define _HOLBERTON_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
