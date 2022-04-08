6ea55dabe6d3f7c05dfb108eac4581ed619ef1f5
Caryourdaysbm <adeyemioluwakayode73@gmail.com>
2022-04-07 19:41:48 -0700

    malloc free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,18 +11,21 @@
 void _is_zero(char *argv[])
 {
 int i, isn1 = 1, isn2 = 1;
+
 for (i = 0; argv[1][i]; i++)
 if (argv[1][i] != '0')
 {
 isn1 = 0;
 break;
 }
+
 for (i = 0; argv[2][i]; i++)
 if (argv[2][i] != '0')
 {
 isn2 = 0;
 break;
 }
+
 if (isn1 == 1 || isn2 == 1)
 {
 printf("0\n");
@@ -39,6 +42,7 @@ exit(0);
 char *_initialize_array(char *ar, int lar)
 {
 int i = 0;
+
 for (i = 0; i < lar; i++)
 ar[i] = '0';
 ar[lar] = '\0';
@@ -55,6 +59,7 @@ return (ar);
 int _checknum(char *argv[], int n)
 {
 int ln;
+
 for (ln = 0; argv[n][ln]; ln++)
 if (!isdigit(argv[n][ln]))
 {
@@ -75,6 +80,7 @@ int main(int argc, char *argv[])
 {
 int ln1, ln2, lnout, add, addl, i, j, k, ca;
 char *nout;
+
 if (argc != 3)
 printf("Error\n"), exit(98);
 ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
@@ -84,7 +90,7 @@ printf("Error\n"), exit(98);
 nout = _initialize_array(nout, lnout);
 k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 for (; k >= 0; k--, i--)
-{
+    {
 if (i < 0)
 {
 if (addl > 0)
