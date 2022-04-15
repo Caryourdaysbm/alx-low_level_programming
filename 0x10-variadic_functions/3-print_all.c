08f2c978085f3f5c21697d19e812f9996d15ae1a
Caryourdaysbm <adeyemioluwakayode73@gmail.com>
2022-04-14 23:14:32 -0700

    variadic

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,80 +1,49 @@
+#include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
-/**
- * print_c - Prints char
- * @list: arguments
- * Return: void
- */
-void print_c(va_list list)
-{
-printf("%c", (char)va_arg(list, int));
-}
-/**
- * print_d - Prints digit
- * @list: arguments
- * Return: void
- */
-void print_d(va_list list)
-{
-printf("%d", va_arg(list, int));
-}
-/**
- * print_f - Prints float
- * @list: arguments
- * Return: void
- */
-void print_f(va_list list)
-{
-printf("%f", (float)va_arg(list, double));
-}
-/**
- * print_str - Prints str
- * @list: arguments
- * Return: void
- */
-void print_s(va_list list)
-{
-char *str = va_arg(list, char *);
-
-if (str)
-{
-printf("%s", str);
-return;
-}
-printf("(nil)");
-}
 
 /**
- * print_all - Prints anything
- * @format: format to print
- * Return: void
- */
+* print_all - print char, integer, float and string
+* @format: format
+*/
 void print_all(const char * const format, ...)
 {
 va_list list;
-char *separator = "";
-int i = 0, j;
-
-filter filt[] = {
-{'c', print_c},
-{'i', print_d},
-{'f', print_f},
-{'s', print_s}
-};
+unsigned int j = 0, start = 0;
+char *p;
 va_start(list, format);
-while (format && format[i])
+while (format && format[j] != '\0')
 {
-j = 0;
-while (j < 4)
-{
-if (format[i] == filt[j].fmt)
-{
-printf("%s", separator);
-filt[j].f(list);
-separator = ", ";
-}
+switch (format[j])
+{ case 'c':
+switch (start)
+{ case 1: printf(", "); }
+start = 1;
+printf("%c", va_arg(list, int));
+break;
+case 'i':
+switch (start)
+{ case 1: printf(", "); }
+start = 1;
+printf("%i", va_arg(list, int));
+break;
+case 'f':
+switch (start)
+{ case 1: printf(", "); }
+start = 1;
+printf("%f", va_arg(list, double));
+break;
+case's':
+switch (start)
+{ case 1: printf(", "); }
+start = 1;
+p = va_arg(list, char*);
+if (p)
+{ printf("%s", p);
+break; }
+printf("%p", p);
+break; }
 j++;
-}
-i++;
 }
 printf("\n");
 va_end(list);
